3981a15e336ee811835ecc1e268cf28974cb22f6
mortenjull <mortenjull@hotmail.com>
2018-11-12 09:43:04 +0100

    initial commit

---
+++ IOTBartendBotAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IOTBartendBotAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
